9a3758a7469d91d00e4a919f66a650416270cc36
Jharwin Barrozo <jharwinbarrozo@gmail.com>
2019-03-19 08:31:37 +0800

    Update receive.sh

--- receive.sh
+++ receive.sh
@@ -42,10 +42,11 @@ fi
 # $5 = EPOC start time
 # $6 = Time to capture
 # $7 = Satellite max elevation
-
+AMP_GAIN=45
+PPM_CORR=0
 START_DATE=$(date '+%d-%m-%Y %H:%M')
 FOLDER_DATE="$(date +%Y)/$(date +%m)/$(date +%d)"
-timeout "${6}" /usr/local/bin/rtl_fm -f "${2}"M -s 60k -g 50 -p 55 -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
+timeout "${6}" /usr/local/bin/rtl_fm -f "${2}"M -s 60k -g $AMP_GAIN -p $PPM_CORR -E wav -E deemp -F 9 - | /usr/bin/sox -t raw -e signed -c 1 -b 16 -r 60000 - ${NOAA_AUDIO}/audio/"${3}".wav rate 11025
 
 PASS_START=$(expr "$5" + 90)
 SUN_ELEV=$(python2 sun.py $PASS_START)
@@ -64,14 +65,5 @@ fi
 for i in $ENHANCEMENTS; do
 	/usr/local/bin/wxtoimg -o -m ${NOAA_HOME}/map/"${3}"-map.png -e $i ${NOAA_AUDIO}/audio/"${3}".wav ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg
 	/usr/bin/convert -quality 90 -format jpg ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg -undercolor black -fill yellow -pointsize 18 -annotate +20+20 "${1} $i ${START_DATE}" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg
-	# GOOGLE DRIVE!!!! 
-	# /usr/bin/gdrive upload --parent 1gehY-0iYkNSkBU9RCDsSTexRaQ_ukN0A ${NOAA_OUTPUT}/image/${FOLDER_DATE}/"${3}"-$i.jpg
 done
 
-if [ "${SUN_ELEV}" -gt "${SUN_MIN_ELEV}" ]; then
-	python2 ${NOAA_HOME}/post.py "$1 ${START_DATE}" "$7" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-MCIR-precip.jpg ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-MSA-precip.jpg ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-HVC-precip.jpg ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-HVCT-precip.jpg 
-else
-	python2 ${NOAA_HOME}/post.py "$1 ${START_DATE}" "$7" ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-MCIR-precip.jpg ${NOAA_OUTPUT}/image/${FOLDER_DATE}/$3-MCIR.jpg 
-fi
-
-rm ${NOAA_AUDIO}/audio/*
